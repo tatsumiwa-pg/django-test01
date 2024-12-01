3161ef23486ccc97500049d0afcf831c437923c7
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2024-12-01 20:29:23 +0900

    rename app_config/view.py => app_config/views.py

--- app_config/views.py
+++ app_config/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
 
 class Index(View):
-    def get(self, request: dict, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         return redirect(reverse('app_folder:top'))
 index = Index.as_view()
