f098073f2a07427e06c47f40d98b2599fcff149a
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2024-12-05 02:58:20 +0900

    add idnex into top_page

--- app_folder/views.py
+++ app_folder/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Memo
 
 class SampleView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'app_folder/top.html')
+        result = Memo.objects.all()
+        context ={'result':result}
+        return render(request, 'app_folder/top.html', context=context,)
 top = SampleView.as_view()
